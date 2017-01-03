6847968294cb770b072d17ad2fc309419f5265f0
Stephen Webb <spidey@gopherspidey.net>
2017-01-02 23:27:56 -0600

    Figured out the Server Client configuration in the same code base that works.

---
+++ server.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+
+const express = require('express');
+
+const app = express();
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.listen(app.get('port'), () => {
+  console.log(`App listening on port: ${app.get('port')}`);
+});
